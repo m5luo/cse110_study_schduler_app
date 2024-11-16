d2befb130f86a53e69b877c845a0a9c833f607fe
m5luo <m5luo@ucsd.edu>
2024-11-15 17:53:23 -0800

    signup integrated with backend

---
+++ client/src/types/types.ts
@@ -0,0 +1,12 @@
+export type User = {
+    username: string;
+    password: string;
+    email: string;
+};
+
+export type UserCredentials = {
+    id: number,
+    username: string,
+    password: string,
+    token: string
+}
