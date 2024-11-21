b8c1b16fc983096a1b7539adc67169543e04cb61
JonathanXiang <110417489+JonathanXiang@users.noreply.github.com>
2024-11-21 09:39:28 -0800

    Test Fail Resolve

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('save event button exists', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Sign in/i);
-  expect(linkElement).toBeInTheDocument();
+  const saveButton = screen.getByText(/Save/i);
+  expect(saveButton).toBeInTheDocument();
 });
