3662c2d4ffa5c4c3e15578772be7b0ee62ff092f
PVcookie <jejhan@ucsd.edu>
2024-11-27 10:34:25 -0800

    created notes editing UI with sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
 import ForgotPasswordForm from './ForgotPasswordForm';
+import NotesPage from './NotesPage';
 
 const App: React.FC = () => {
   return (
@@ -10,7 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<SignInForm />} />
         <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/forgot-password" element={<ForgotPasswordForm />} /> {/* Add this route */}
+        <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+        <Route path="/notes" element={<NotesPage />} />
       </Routes>
     </Router>
   );
